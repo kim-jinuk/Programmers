ba9a283a6f9081f6b58214eb0af0e132f0d99d52
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-01 21:04:05 +0900

    [level 2] Title: 할인 행사, Time: 106.67 ms, Memory: 12 MB -BaekjoonHub

--- C++/프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ C++/프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -4,36 +4,22 @@
 
 using namespace std;
 
-vector<string> create_vector(const vector<string>& want, const vector<int>& number) {
-    vector<string> arr;
-    for (int i = 0; i < want.size(); ++i) {
-        for (int j = 0; j < number[i]; ++j) {
-            arr.push_back(want[i]);
-        }
-    }
-    return arr;
-}
-
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
-    vector<string> want_arr = create_vector(want, number);
-    int n = want_arr.size();
+    unordered_map<string, int> wantMap;
     
-    for (int i = 0; i < discount.size() - n + 1; ++i) {
-        unordered_map<string, int> discount_map;
-        for (int j = 0; j < n; ++j) {
-            discount_map[discount[i + j]]++;
+    for (int i = 0; i < want.size(); ++i) {
+        wantMap[want[i]] = number[i];
+    }
+    
+    for (int i = 0; i < discount.size() - 9; ++i) {
+        unordered_map<string, int> discount_10d;
+        
+        for (int j = i; j < 10 + i; ++j) {
+            discount_10d[discount[j]]++;
         }
-        for (int j = 0; j < n; ++j) {
-            discount_map[want_arr[j]]--;
-        }
-        for (int j = 0; j < n; ++j) {
-            if (discount_map[want_arr[j]] != 0) {
-                answer--;
-                break;
-            }
-        }
-        answer++;
+        
+        if (wantMap == discount_10d) answer++;
     }
     
     return answer;
