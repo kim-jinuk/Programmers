9112477bfd7369ed435571d42a714d2c148e37f2
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-02 09:28:03 -0700

    [level 3] Title: 베스트앨범, Time: 0.04 ms, Memory: 4.17 MB -BaekjoonHub

--- C++/프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ C++/프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,38 +1,38 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
-bool compareGenre(const pair<string, int>& a, const pair<string, int>& b) {
+bool sort_genres_play(const pair<string, int>& a, const pair<string, int>& b) {
     return a.second > b.second;
 }
 
-bool compareSong(const pair<int, int>& a, const pair<int, int>& b) {
+bool sort_genres_songs(const pair<int, int>& a, const pair<int, int>& b) {
     if (a.second == b.second) return a.first < b.first;
     return a.second > b.second;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
-    unordered_map<string, vector<pair<int, int>>> genres_dict;
-    unordered_map<string, int> play_dict;
+    unordered_map<string, int> genres_play;
+    unordered_map<string, vector<pair<int, int>>> genres_songs;
     
     for (int i = 0; i < genres.size(); ++i) {
-        genres_dict[genres[i]].push_back({i, plays[i]});
-        play_dict[genres[i]] += plays[i];
+        genres_play[genres[i]] += plays[i];
+        genres_songs[genres[i]].push_back({i, plays[i]});
     }
     
-    vector<pair<string, int>> sorted_genres(play_dict.begin(), play_dict.end());
-    sort(sorted_genres.begin(), sorted_genres.end(), compareGenre);
+    vector<pair<string, int>> sorted_genres_play(genres_play.begin(), genres_play.end());
+    sort(sorted_genres_play.begin(), sorted_genres_play.end(), sort_genres_play);
     
-    for (auto& genre : sorted_genres) {
-        auto& songs = genres_dict[genre.first];
-        sort(songs.begin(), songs.end(), compareSong);
+    for (const pair<string, int>& a : sorted_genres_play) {
+        vector<pair<int, int>> b(genres_songs[a.first].begin(), genres_songs[a.first].end());
+        sort(b.begin(), b.end(), sort_genres_songs);
         
-        for (int i = 0; i < min(2, (int)songs.size()); ++i) {
-            answer.push_back(songs[i].first);
+        for (int i = 0; i < min(2, (int)b.size()); ++i) {
+            answer.push_back(b[i].first);
         }
     }
     
