5f280b5c4736efc8cf0f4bd906e5aa6499c8d12f
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-03 08:27:30 -0700

    [level 3] Title: 베스트앨범, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ C++/프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -5,34 +5,34 @@
 
 using namespace std;
 
-bool sort_genres_play(const pair<string, int>& a, const pair<string, int>& b) {
+bool sort_genres_count(const pair<string, int>& a, const pair<string, int>& b) {
     return a.second > b.second;
 }
 
-bool sort_genres_songs(const pair<int, int>& a, const pair<int, int>& b) {
+bool sort_songs(const pair<int, int>& a, const pair<int, int>& b) {
     if (a.second == b.second) return a.first < b.first;
     return a.second > b.second;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
-    unordered_map<string, int> genres_play;
-    unordered_map<string, vector<pair<int, int>>> genres_songs;
+    unordered_map<string, int> genres_count;
+    unordered_map<string, vector<pair<int, int>>> songs;
     
     for (int i = 0; i < genres.size(); ++i) {
-        genres_play[genres[i]] += plays[i];
-        genres_songs[genres[i]].push_back({i, plays[i]});
+        genres_count[genres[i]] += plays[i];
+        songs[genres[i]].push_back({i, plays[i]});
     }
     
-    vector<pair<string, int>> sorted_genres_play(genres_play.begin(), genres_play.end());
-    sort(sorted_genres_play.begin(), sorted_genres_play.end(), sort_genres_play);
+    vector<pair<string, int>> sorted_genres_count(genres_count.begin(), genres_count.end());
+    sort(sorted_genres_count.begin(), sorted_genres_count.end(), sort_genres_count);
     
-    for (const pair<string, int>& a : sorted_genres_play) {
-        vector<pair<int, int>> b(genres_songs[a.first].begin(), genres_songs[a.first].end());
-        sort(b.begin(), b.end(), sort_genres_songs);
+    for (pair<string, int>& a : sorted_genres_count) {
+        vector<pair<int, int>> sorted_songs(songs[a.first].begin(), songs[a.first].end());
+        sort(sorted_songs.begin(), sorted_songs.end(), sort_songs);
         
-        for (int i = 0; i < min(2, (int)b.size()); ++i) {
-            answer.push_back(b[i].first);
+        for (int i = 0; i < min(2, (int)sorted_songs.size()); ++i) {
+            answer.push_back(sorted_songs[i].first);
         }
     }
     
