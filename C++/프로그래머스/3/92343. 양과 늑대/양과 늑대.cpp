d4b9ef3334d44910a928eec6406adc53cb646aec
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-14 23:01:22 +0900

    [level 3] Title: 양과 늑대, Time: 1.79 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ C++/프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> tree;
+vector<int> visited, comp;
+int n, answer = 0;
+
+void dfs(vector<int> cur) {
+    int sheep = 0, wolf = 0;
+    for (int c : cur) {
+        if (comp[c] == 1) wolf++;
+        else sheep++;
+    }
+    
+    if (sheep <= wolf) return;
+    
+    answer = max(answer, sheep);
+    
+    for (int i = 0; i < cur.size(); ++i) {
+        int node = cur[i];
+        for (int g : tree[node]) {
+            if (visited[g]) continue;
+            visited[g] = true;
+            cur.push_back(g);
+            dfs(cur);
+            cur.pop_back();
+            visited[g] = false;
+        }
+    }
+}
+
+int solution(vector<int> info, vector<vector<int>> edges) {
+    n = info.size();
+    tree.resize(n);
+    visited.resize(n, false);
+    comp = info;
+    
+    for (auto e : edges) {
+        tree[e[0]].push_back(e[1]);
+    }
+    
+    visited[0] = true;
+    
+    dfs({0});
+    return answer;
+}
