a2275f33c8636788c3011400e6260e15d7797526
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-14 20:31:18 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.09 MB -BaekjoonHub

--- C++/프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ C++/프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,26 +1,41 @@
-#include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-void dfs(int i, vector<int>& visited, const vector<vector<int>>& computers) {
-    visited[i] = 1;
-    
-    for (int j = 0; j < computers[i].size(); ++j) {
-        if (computers[i][j] == 1 && visited[j] == 0) {
-            dfs(j, visited, computers);
-        }
-    }
+int parent[201];
+
+int findParent(int node) {
+    if (parent[node] == node) return node;
+    return parent[node] = findParent(parent[node]);
+}
+
+void unionNodes(int node1, int node2) {
+    int root1 = findParent(node1);
+    int root2 = findParent(node2);
+    if (root1 != root2) parent[root2] = root1;
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    vector<int> visited(n, 0);
+    for (int i = 0; i < n; ++i) {
+        parent[i] = i;
+    }
     
     for (int i = 0; i < n; ++i) {
-        if (visited[i] == 0) {
-            dfs(i, visited, computers);
+        for (int j = 0; j < n; ++j) {
+            if (computers[i][j]) {
+                unionNodes(i, j);
+            }
+        }
+    }
+    
+    unordered_map<int, bool> networkFound;
+    for (int i = 0; i < n; ++i) {
+        int root = findParent(i);
+        if (!networkFound[root]) {
             answer++;
+            networkFound[root] = true;
         }
     }
     
