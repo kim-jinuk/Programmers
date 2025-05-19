079bd756006798c8cbd8992630ce1be13cc5ede9
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-19 11:48:43 +0900

    [level 2] Title: 양궁대회, Time: 0.15 ms, Memory: 4.16 MB -BaekjoonHub

--- C++/프로그래머스/2/92342. 양궁대회/README.md
+++ C++/프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.15 ms
+메모리: 4.16 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 19일 10:02:10
+2025년 05월 19일 11:48:42
 
 ### 문제 설명
 
--- C++/프로그래머스/2/92342. 양궁대회/양궁대회.cpp
+++ C++/프로그래머스/2/92342. 양궁대회/양궁대회.cpp
@@ -7,51 +7,54 @@ vector<int> answer = {-1};
 vector<int> Info;
 int best_score = 0;
 
-int get_score(const vector<int>& cur) {
-    int ryan = 0, apeach = 0;
-    for (int i = 0; i < cur.size(); ++i) {
-        if (cur[i] == 0 && Info[i] == 0) continue;
-        if (cur[i] > Info[i]) ryan += (10 - i);
-        else apeach += (10 - i);
-    }
-    return ryan - apeach;
-}
-
-bool is_better(const vector<int>& a, const vector<int>& b) {
+bool isbetter(vector<int> a, vector<int> b) {
     for (int i = 10; i >= 0; --i) {
         if (a[i] != b[i]) return a[i] > b[i];
     }
+    return false;
+}
+
+int get_score(const vector<int>& cur) {
+    int diff = 0;
+    for (int i = 0; i < cur.size(); ++i) {
+        if (cur[i] == 0 && Info[i] == 0) continue;
+        if (cur[i] > Info[i]) diff += (10 - i);
+        else diff -= (10 - i);
+    }
+    return diff;
 }
 
 void dfs(int N, int pos, vector<int> cur) {
     if (N == 0 || pos == 11) {
-        if (N > 0) cur[10] += N;
+        cur[10] += N;
         int diff = get_score(cur);
-        if (diff > 0) {
-            if (diff > best_score) {
-                best_score = diff;
-                answer = cur;
-            }
-            else if (diff == best_score && is_better(cur, answer)) {
-                answer = cur;
-            }
+        if (diff <= 0) {
+            cur[10] -= N;
+            return;
         }
-        if (N > 0) cur[10] -= N;
+        
+        if (best_score < diff || (best_score == diff && isbetter(cur, answer))) {
+            best_score = diff;
+            answer = cur;
+        }
+
+        cur[10] -= N;
         return;
     }
     
-    if (N > Info[pos]) {
+    if (N >= Info[pos] + 1) {
         cur[pos] = Info[pos] + 1;
-        dfs(N - cur[pos], pos + 1, cur);
+        dfs(N - (Info[pos] + 1), pos + 1, cur);
         cur[pos] = 0;
     }
+    
     dfs(N, pos + 1, cur);
 }
 
 vector<int> solution(int n, vector<int> info) {
     Info = info;
     vector<int> cur(info.size(), 0);
-    dfs(n, 0, cur);
     
+    dfs(n, 0, cur);
     return answer;
 }
