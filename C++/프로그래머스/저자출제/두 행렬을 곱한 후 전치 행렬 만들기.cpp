f6d4c397f2be163690f2cc60a56d43bb43f52632
kimjinuk <gjm8856@gmail.com>
2025-05-20 22:04:34 +0900

    add matrix mul example

--- C++/프로그래머스/저자출제/두 행렬을 곱한 후 전치 행렬 만들기.cpp
+++ C++/프로그래머스/저자출제/두 행렬을 곱한 후 전치 행렬 만들기.cpp
@@ -3,15 +3,34 @@
 
 using namespace std;
 
-vector<vector<int>> solution(vector<vector<int>> matrix1, vector<vector<int>> maxrix2) {
-	vector<vector<int>> answer(3, vector<int>(3, 0));
+vector<vector<int>> multiply(vector<vector<int>> matrix1, vector<vector<int>> matrix2) {
+	vector<vector<int>> result(3, vector<int>(3, 0));
 	for (int i = 0; i < 3; ++i) {
 		for (int j = 0; j < 3; ++j) {
 			for (int k = 0; k < 3; ++k) {
-				answer[j][i] += matrix1[i][k] * matrix2[k][j];
+				result[i][j] += matrix1[i][k] * matrix2[k][j];
 			}
 		}
 	}
+
+	return result;
+}
+
+vector<vector<int>> transpose(vector<vector<int>> matrix) {
+	vector<vector<int>> result(3, vector<int>(3, 0));
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			result[i][j] = matrix[j][i];
+		}
+	}
+
+	return result;
+}
+
+vector<vector<int>> solution(vector<vector<int>> matrix1, vector<vector<int>> maxrix2) {
+	vector<vector<int>> answer(3, vector<int>(3, 0));
+	answer = multiply(matrix1, matrix2);
+	answer = transpose(answer);
 
 	return answer;
 }
