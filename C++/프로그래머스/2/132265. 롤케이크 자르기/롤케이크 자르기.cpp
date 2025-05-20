c5ad36e7413d9415c483f79b0d952226dd78dac9
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-21 08:56:05 +0900

    [level 2] Title: 롤케이크 자르기, Time: 51.61 ms, Memory: 36.8 MB -BaekjoonHub

--- C++/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ C++/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 int solution(vector<int> topping) {
     int answer = 0;
-    unordered_map<int, int> left_map, right_map;
+    unordered_map<int, int> left, right;
     
-    for (int i : topping) right_map[i]++;
+    for (int i : topping) {
+        right[i]++;
+    }
     
-    for (int i = 0; i < topping.size(); ++i) {
-        int t = topping[i];
-        left_map[t]++;
-        if (--right_map[t] == 0) right_map.erase(t);
-        if (left_map.size() == right_map.size()) answer++;
+    for (int i : topping) {
+        left[i]++;
+        if (--right[i] == 0) right.erase(i);
+        if (left.size() == right.size()) answer++;
     }
     
     return answer;
