63cccf84ce61e542944de7d965d29e3eb0950896
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-21 08:52:46 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.14 ms, Memory: 3.78 MB -BaekjoonHub

--- C++/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ C++/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-string get_binary(int x) {
-    if (x == 1) return "1";
-    return get_binary(x / 2) + to_string(x % 2);
+string binary_transpose(int value) {
+    if (value == 1) return "1";
+    return binary_transpose(value / 2) + to_string(value % 2);
 }
 
 vector<int> solution(string s) {
@@ -14,13 +14,12 @@ vector<int> solution(string s) {
     int result = 0;
     
     while (s != "1") {
-        int s_length = s.length();
         int temp_zero_count = 0;
         for (char c : s) {
             if (c == '0') temp_zero_count++;
         }
         zero_count += temp_zero_count;
-        s = get_binary(s_length - temp_zero_count);
+        s = binary_transpose(s.length() - temp_zero_count);
         result++;
     }
     
