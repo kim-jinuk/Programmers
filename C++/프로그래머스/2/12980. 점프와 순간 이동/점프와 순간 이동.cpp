78fa340a3047cc5f2b32c1dee3f75a6be86fa881
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-21 10:00:29 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.54 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    while (n != 0) {
+        if (n % 2 == 1) {
+            answer++;
+            n--;
+        }
+        n /= 2;
+    }
+
+    return answer;
+}
