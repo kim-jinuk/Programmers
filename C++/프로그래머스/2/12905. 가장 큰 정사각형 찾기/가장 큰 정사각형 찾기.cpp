efcdaf083b76e395f58fb5e6b2b8e592e2eb1bc9
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-05-22 14:50:55 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 6.62 ms, Memory: 42.6 MB -BaekjoonHub

--- C++/프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ C++/프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -9,7 +9,6 @@ int solution(vector<vector<int>> board)
     int m = board[0].size();
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
     
-    dp[1][1] = 1;
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j) {
             if (board[i - 1][j - 1] == 1) {
