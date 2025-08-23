6661ac7f62bf7cf9c0313f4e1f52f1bdc6af629d
JinUk Kim <88013805+kim-jinuk@users.noreply.github.com>
2025-08-23 18:50:37 +0900

    [level 3] Title: 합승 택시 요금, Time: 2.30 ms, Memory: 7.6 MB -BaekjoonHub

---
+++ C++/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <limits>
+
+using namespace std;
+const int INF = numeric_limits<int>::max();
+
+vector<int> dijkstra(int start, int n, const vector<vector<int>>& arr) {
+    vector<int> result(n+1, INF);
+    vector<bool> visited(n+1);
+    result[start] = 0;
+    
+    for (int i = 0; i <= n; ++i) {
+        int min_value = INF, curr = -1;
+        for (int j = 0; j <= n; ++j) {
+            if (!visited[j] && result[j] < min_value) {
+                min_value = result[j];
+                curr = j;
+            }
+        }
+        if (curr == -1) break;
+        visited[curr] = true;
+        
+        for (int j = 0; j <= n; ++j) {
+            if (!visited[j] && arr[curr][j] != INF) {
+                if (result[j] > arr[curr][j] + result[curr]) {
+                    result[j] = arr[curr][j] + result[curr];
+                }
+            }
+        }
+    }
+    
+    return result;
+}
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    int answer = INF;
+    vector<vector<int>> arr(n + 1, vector<int>(n + 1, INF));
+    
+    for (int i = 0; i <= n; ++i) {
+        arr[i][i] = 0;
+    }
+    
+    for (vector<int> num : fares) {
+        arr[num[0]][num[1]] = num[2];
+        arr[num[1]][num[0]] = num[2];
+    }
+    
+    vector<int> result_s = dijkstra(s, n, arr);
+    vector<int> result_a = dijkstra(a, n, arr);
+    vector<int> result_b = dijkstra(b, n, arr);
+    
+    for (int i = 1; i <= n; ++i) {
+        if (result_s[i] != INF && result_a[i] != INF && result_b[i] != INF) {
+            answer = min(answer, result_s[i] + result_a[i] + result_b[i]);
+        }
+    }
+    
+    return answer;
+}
