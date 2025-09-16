a5a41b1fcf2c5d6ef13b3443c14c84c0057629ee
JinUk Kim <88013805+kim-jinuk@users.noreply.github.com>
2025-09-16 13:51:26 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ C++/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
-bool comp_char(const char& a, const char& b) {
+bool compare(long long a, long long b) {
     return a > b;
 }
 
 long long solution(long long n) {
-    string s = to_string(n);
-    sort(s.begin(), s.end(), comp_char);
-    long long answer = stoll(s);
+    long long answer = 0;
+    vector<long long> vec;
+    
+    while (n != 0) {
+        vec.push_back(n % 10);
+        n /= 10;
+    }
+    sort(vec.begin(), vec.end(), compare);
+    
+    for (long long val : vec) {
+        answer *= 10;
+        answer += val;
+    }
+    
     return answer;
 }
