66dc93e031979ae86a6d91db94ca5825f4b83ff9
JinUk Kim <88013805+kim-jinuk@users.noreply.github.com>
2025-09-16 16:38:02 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    while (num != 1) {
+        if (answer > 400) return -1;
+        answer++;
+        
+        if (num % 2 == 0) num /= 2;
+        else {num = num * 3 + 1;}
+    }
+    return answer;
+}
