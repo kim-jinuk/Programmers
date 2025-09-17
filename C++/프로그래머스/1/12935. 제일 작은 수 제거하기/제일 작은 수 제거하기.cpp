1f26ceca1422f82dc87801e3d6bd0bd9a0724f3c
JinUk Kim <88013805+kim-jinuk@users.noreply.github.com>
2025-09-17 14:19:08 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 10.83 ms, Memory: 13.3 MB -BaekjoonHub

--- C++/프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ C++/프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -1,21 +1,13 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
     if (arr.size() < 2) return vector(1, -1);
     
-    int min_val = arr[0];
-    
-    for (int i = 1; i < arr.size(); ++i) {
-        if (arr[i] < min_val) {
-            min_val = arr[i];
-        }
-    }
-    
+    int min_val = *min_element(arr.begin(), arr.end());
     arr.erase(remove(arr.begin(), arr.end(), min_val), arr.end());
     
     return arr;
